4c6c69ce335fee8f9ff983412108635c5fcefd96
Wyatt Phillips <phillipsw1@wit.edu>
2020-04-11 01:57:53 -0400

    Updates

---
+++ precache-manifest.f144066b924861bb1cf140fbf45f33f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8920d706786e7ebb5cca929378cd12e",
+    "url": "/react-bootcamp/index.html"
+  },
+  {
+    "revision": "7759076a42434c7ff789",
+    "url": "/react-bootcamp/static/css/main.73fc47dc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-bootcamp/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bootcamp/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7759076a42434c7ff789",
+    "url": "/react-bootcamp/static/js/main.e5b487d1.chunk.js"
+  },
+  {
+    "revision": "4e09a84f083d5e6e4581",
+    "url": "/react-bootcamp/static/js/runtime-main.5d0d1d95.js"
+  },
+  {
+    "revision": "ca20807d60a26acef9ba8adf94bab100",
+    "url": "/react-bootcamp/static/media/tommy.ca20807d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters-rolodex/precache-manifest.3f123631f50c6ebe8257c1b75ecdcdf5.js"
+  "/react-bootcamp/precache-manifest.f144066b924861bb1cf140fbf45f33f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-bootcamp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
