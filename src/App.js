cdfe4e470e1d4acf0839e1a28392f196e504695e
Wyatt Phillips <phillipsw1@wit.edu>
2020-04-10 22:55:26 -0400

    Use fetching

--- src/App.js
+++ src/App.js
@@ -9,20 +9,48 @@ class App extends Component {
     this.state = {
       name: 'Tommy',
       drink: 'Soda',
-      consumed: 0
+      limit: 8,
+      consumed: 0,
+
+      monsters: []
     }
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {
+          this.setState({monsters: users})
+        })
+  }
+
   render() {
+
     return (
         <div className="App">
-          <header className="App-header">
-            <img src={tommy} className="App-logo" alt="logo" />
-            <p>
-              {this.state.name} drank {this.state.consumed} {this.state.drink}{(this.state.consumed != 1) ? "s" : ""} for lunch.
-            </p>
-            <button onClick={() => this.setState({consumed: this.state.consumed + 1})}>Earn PlayCoin</button>
-          </header>
+          <div className="Tommy">
+            <header className="App-header">
+              <img src={tommy} className="App-logo" alt="logo" />
+              <p>
+                {this.state.name} drank {this.state.consumed} {this.state.drink}{(this.state.consumed != 1) ? "s" : ""} for lunch.
+              </p>
+              <button onClick={() => {
+                if (this.state.consumed < this.state.limit) {
+                  this.setState({consumed: this.state.consumed + 1})
+                } else {
+                  console.log('Tommy has had enough soda!')
+                }}} disabled={this.state.consumed >= this.state.limit}>{(this.state.consumed < this.state.limit) ? "Drink Soda" : "Gordon, Im full"}</button>
+
+              <div className="monsters d-block">
+
+                <h3>Credits</h3>
+                <hr/>
+                {
+                  this.state.monsters.map(value => <p key={value.id}> {value.name} </p>)
+                }
+              </div>
+            </header>
+          </div>
         </div>
     );
   }
