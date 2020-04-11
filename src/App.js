d42ae71cb50f66ac75ae7ad0869178e5aad7e77c
Wyatt Phillips <phillipsw1@wit.edu>
2020-04-10 21:19:24 -0400

    Add Tommy Landingpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import tommy from './img/tommy.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={tommy} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I drink <b>Soda</b> for lunch
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Kill Me Now
         </a>
       </header>
     </div>
